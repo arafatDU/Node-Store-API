f0d2e6795f6313e71039f34734eeca4b7e926071
ARAFAT <munshiarafathussain@gmail.com>
2024-03-26 00:44:23 +0600

    Database connected

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongodb_connect = process.env.MONGO_URI;
 
 // middleware
 app.use(express.json());
@@ -13,8 +15,16 @@ app.get('/', (req, res) => {
   res.send('<h1>Store Manager API</h1>');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 
+
+mongoose.connect(mongodb_connect)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
