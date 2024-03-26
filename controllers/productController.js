9b142d91da204e9afc262980b3e77ba96bb921b3
ARAFAT <munshiarafathussain@gmail.com>
2024-03-26 17:24:16 +0600

    numeric Filters done & Tested

--- controllers/productController.js
+++ controllers/productController.js
@@ -33,9 +33,21 @@ const getAllProductsStatic3 = asyncWrapper( async (req, res) => {
 
 
 
+// numeric filters
+const getAllProductsStatic4 = asyncWrapper( async (req, res) => {
+
+  const products = await Product.find({price: { $gt: 30 }})
+    .sort('price')
+    .select('name price')
+    .limit(30)
+    .skip(0);
+  res.status(200).json({ products , nbHits: products.length });
+})
+
+
 
 const getAllProducts = asyncWrapper( async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   // query
@@ -49,6 +61,32 @@ const getAllProducts = asyncWrapper( async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i'};
   }
   console.log(queryObject);
+  
+
+  // numericFilters
+  if(numericFilters){
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+    const regEx = /\b(<|>|<=|>=|=)\b/g;
+    let filters = numericFilters.replace(
+      regEx, 
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+    console.log(queryObject);
+  }
+
   let result = Product.find(queryObject);
 
   // sort
@@ -69,7 +107,7 @@ const getAllProducts = asyncWrapper( async (req, res) => {
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
-  console.log('skip:', skip, 'limit:', limit);
+  //console.log('skip:', skip, 'limit:', limit);
   result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ products , nbHits: products.length });
@@ -80,5 +118,6 @@ module.exports = {
   getAllProducts,
   getAllProductsStatic,
   getAllProductsStatic2,
-  getAllProductsStatic3
+  getAllProductsStatic3,
+  getAllProductsStatic4
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllProducts, getAllProductsStatic, getAllProductsStatic2, getAllProductsStatic3 } = require('../controllers/productController.js');
+const { getAllProducts, getAllProductsStatic, getAllProductsStatic2, getAllProductsStatic3, getAllProductsStatic4 } = require('../controllers/productController.js');
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.route('/').get(getAllProducts);
 router.route('/static').get(getAllProductsStatic);
 router.route('/static2').get(getAllProductsStatic2);
 router.route('/static3').get(getAllProductsStatic3);
+router.route('/static4').get(getAllProductsStatic4);
 
 
 module.exports = router;
