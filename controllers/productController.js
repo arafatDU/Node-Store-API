cdc3bc37afe2a0a4fd506cad4db0e8e2b70f205c
ARAFAT <munshiarafathussain@gmail.com>
2024-03-26 14:39:06 +0600

    sort, limit, skip, select done

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,8 @@
 const asyncWrapper = require('../middleware/async.js');
 const Product = require('../models/productModel.js');
 
+
+// search
 const getAllProductsStatic = asyncWrapper( async (req, res) => {
   const search = 'ab';
   const products = await Product.find({
@@ -10,9 +12,33 @@ const getAllProductsStatic = asyncWrapper( async (req, res) => {
 })
 
 
+// sort
+const getAllProductsStatic2 = asyncWrapper( async (req, res) => {
+
+  const products = await Product.find({}).sort('-name price').select('name price');
+  res.status(200).json({ products , nbHits: products.length });
+})
+
+
+// limit, skip, page
+const getAllProductsStatic3 = asyncWrapper( async (req, res) => {
+
+  const products = await Product.find({})
+    .sort('name price')
+    .select('name price')
+    .limit(5)
+    .skip(1);
+  res.status(200).json({ products , nbHits: products.length });
+})
+
+
+
+
 const getAllProducts = asyncWrapper( async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
+
+  // query
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
@@ -23,12 +49,36 @@ const getAllProducts = asyncWrapper( async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i'};
   }
   console.log(queryObject);
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  // sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  }else{
+    result = result.sort('createdAt');
+  }
+
+  // fields
+  if (fields) {
+    const fieldList = fields.split(',').join(' ');
+    result = result.select(fieldList);
+  }
+
+  // limit
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  console.log('skip:', skip, 'limit:', limit);
+  result = result.skip(skip).limit(limit);
+  const products = await result;
   res.status(200).json({ products , nbHits: products.length });
 })
 
 
 module.exports = {
   getAllProducts,
-  getAllProductsStatic
+  getAllProductsStatic,
+  getAllProductsStatic2,
+  getAllProductsStatic3
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getAllProducts, getAllProductsStatic } = require('../controllers/productController.js');
+const { getAllProducts, getAllProductsStatic, getAllProductsStatic2, getAllProductsStatic3 } = require('../controllers/productController.js');
 
 const router = express.Router();
 
 router.route('/').get(getAllProducts);
 router.route('/static').get(getAllProductsStatic);
+router.route('/static2').get(getAllProductsStatic2);
+router.route('/static3').get(getAllProductsStatic3);
 
 
 module.exports = router;
